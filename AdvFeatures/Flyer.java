cf73848a61e29b4d2ef0ff33882f984f68951ea3
Alisa Zarina <85399390+alisazarina@users.noreply.github.com>
2022-04-01 15:28:17 +0800

    Wk7 Lecture -- Example Code

---
+++ AdvFeatures/Flyer.java
@@ -0,0 +1,15 @@
+package AdvFeatures;
+
+/**
+ *
+ * @author Alisa Zarina
+ */
+public interface Flyer {            // for this interface, from the package, right-click New > Java interface [MS: 34:00]
+    
+    public void takeOff();       // cannot have body, so no "{}" -- only method headers
+    
+    public void land();
+    
+    public void fly();
+    
+}
