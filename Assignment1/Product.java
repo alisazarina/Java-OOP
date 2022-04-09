045c56e201168d90d9d2d96c2513205685981862
Alisa Zarina <85399390+alisazarina@users.noreply.github.com>
2022-04-09 23:15:42 +0800

    Add files via upload

--- Assignment1/Product.java
+++ Assignment1/Product.java
@@ -2,90 +2,72 @@ package Assignment1;
 
 /**
  *
- * @author Alisa Zarina
+ * @author 
  */
-
-// Q1-2 Create a Product class with instance variables.
-public class Product {
-    private int productNumber;
+public class Product {      //Product class with instance variables
+    private int itemNumb;   //Instance variable is any variable declared outside any methods & without static keyword
     private String productName;
-    private int productStock;
-    private double productPrice;
+    private int stockQty;
+    private double itemPrice;
     
-    //Q4 Write a getter/accessor and setter/mutator method for each variable.    
-    // Getter/Accessor methods for all variables.
-    public int getProductNumber() {
-        return productNumber;
+    
+    //default constructor, initialize numeric with zeros & String with nulls
+    public Product () {     
+    }
+    
+    //Overloaded constructor, with parameters for all 4 instance variables
+    public Product (int itemNumb, String productName, int stockQty, double itemPrice) {    //Overloaded constructor, initialize parameters
+        this.itemNumb = itemNumb;
+        this.productName = productName;
+        this.stockQty = stockQty;
+        this.itemPrice = itemPrice;
+    }
+    
+    public double calcInventoryValue (int stockQty, double itemPrice) {    //to calculate value of each items   
+        double inventoryValue;
+        
+        inventoryValue = stockQty * itemPrice;
+        
+        return inventoryValue;
+    }
+    
+    @Override          //Override from Object class that will show information of each object
+    public String toString () {
+        return ("Item number: "+itemNumb+", Product Name: "+productName+", Quantity of stock: "+stockQty+", Item price: "+itemPrice);
+//        return ("Product: "+itemNumb+",\t"+productName+",\t"+stockQty+",\t"+String.format("%.2f",itemPrice));
+    }
+        
+    // Getter/Setter methods for all variables ~ allow access & modify/ set private variables
+    public int getItemNumb() {      
+        return itemNumb;
+    }
+
+    public void setItemNumb(int itemNumb) {     
+        this.itemNumb = itemNumb;
     }
 
     public String getProductName() {
         return productName;
     }
-    
-    public int getProductStock() {
-        return productStock;
-    }
 
-    public double getProductPrice() {
-        return productPrice;
-    }
-    
-    
-    // Setter/Mutator method for all variables.
-    public void setProductNumber(int productNumber) {
-        this.productNumber = productNumber;
-    }
-  
     public void setProductName(String productName) {
         this.productName = productName;
     }
-    
-    public void setProductStock(int productStock) {
-        this.productStock = productStock;
-    }
-    
-    public void setProductPrice(double productPrice) {
-        this.productPrice = productPrice;
+
+    public int getStockQty() {
+        return stockQty;
     }
 
-    
-    // Q3(a) Create a default constructor.
-    public Product () {   
-        System.out.println("This is the default constructor.");
-        productNumber = 0;
-        productName = null;
-        productStock = 0;
-        productPrice = 0;
+    public void setStockQty(int stockQty) {
+        this.stockQty = stockQty;
     }
-    
-    
-    // Q3(b) Overload default constructer + create constructor with parameters for all 4 instance variables.  
-    public Product (int productNumber,                                      
-                    String productName,
-                    int productStock,
-                    double productPrice)
-    {
-        this.productNumber = productNumber;
-        this.productName = productName;
-        this.productStock = productStock;
-        this.productPrice = productPrice;       
+
+    public double getItemPrice() {
+        return itemPrice;
     }
-    
-    
-    // Q5 Override the toString() method from the Object class.
-    @Override       
-    public String toString() {
-        return ("Item number: " + productNumber 
-                + "\nName: " + productName 
-                + "\nStock available: " + productStock 
-                + "\nPrice per unit: RM" + productPrice);
+
+    public void setItemPrice(double itemPrice) {
+        this.itemPrice = itemPrice;
     }
-    
-    
-    // Q6 Create a method to calculate the value of each inventory item (= qty on hand (stock) * price).
-    public double inventoryValue(int productStock, double productPrice) {
-        double value = productStock * productPrice;
-        return value;
-    }
-        
 }
+
