5ee0793c3e8d762c55cbc04c6a653ffa5c6803b7
Alisa Zarina <85399390+alisazarina@users.noreply.github.com>
2022-04-27 20:15:47 +0800

    Update Car.java

--- Lab5/Car.java
+++ Lab5/Car.java
@@ -9,24 +9,8 @@ public class Car extends Vehicle implements Transporter {
     private String carType;
     private double ccValue;
     
-    public String getCarType() {
-        return carType;
-    }
-    
-    public void setCarType(String carType) {
-        this.carType = carType;
-    }
-    
-    public double getCcValue() {
-        return ccValue;
-    }
-    
-    public void setCcValue(double ccValue) {
-        this.ccValue = ccValue;
-    }
-    
-    public Car(int year, double price, String type, double cc) {
-    super(year, price);
+    public Car(String type, double cc) {
+    super(1996, 400000.00);
     carType = type;
     ccValue = cc;
     }
@@ -34,14 +18,15 @@ public class Car extends Vehicle implements Transporter {
     @Override
     public void displayVehicle() {
         System.out.println("Car Details:");
-        System.out.println("\nManufactured year: " + super.manufacturedYear);
-        System.out.println("\nPrice : " + super.price);
+        System.out.println("\nManufactured year: " + manufacturedYear);
+        System.out.println("\nPrice : " + price);
         System.out.println("\nType of car: " + carType);
         System.out.println("\nCubic capacity of car: " + ccValue);    
     }
     
     @Override
     public void howToTransport() {
-        System.out.println("\nCar moves on the road...");
+        System.out.println("\nHow do cars move?");
+        System.out.println("\nCars move on the road...");
     }
 }
