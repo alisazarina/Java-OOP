80090bec51dbee0ac53aaf56de399722970c760f
Alisa Zarina <85399390+alisazarina@users.noreply.github.com>
2022-04-27 20:14:22 +0800

    Update Aeroplane.java

--- Lab5/Aeroplane.java
+++ Lab5/Aeroplane.java
@@ -7,40 +7,25 @@ package Lab5;
 
 public class Aeroplane extends Vehicle implements Transporter {
     private String modelNo;
-    private int capacity;
-    
-    public String getModelNo() {
-        return modelNo;
-    }
-    
-    public void setModelNo(String modelNo) {
-        this.modelNo = modelNo;
-    }
-    
-    public int getCapacity() {
-        return capacity;
-    }
-    
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
+    private int capacity; 
     
     public Aeroplane(int year, double price, String modelNo, int capacity) {
-        super(year, price);
+        super(2000, 150000.00);
         this.modelNo = modelNo;
         this.capacity = capacity;
     }
     
     public void displayVehicle() {       
         System.out.println("Aeroplane Details:");
-        System.out.println("\nManufactured year: " + super.manufacturedYear);
-        System.out.println("\nPrice : " + super.price);        
+        System.out.println("\nManufactured year: " + manufacturedYear);
+        System.out.println("\nPrice : " + price);        
         System.out.println("\nModel of aeroplane: " + modelNo);
         System.out.println("\nCapacity: " + capacity);
     }
     
     @Override
     public void howToTransport() {
-        System.out.println("\nAeroplane moves in the air...");
+        System.out.println("\nHow do aeroplanes move?");
+        System.out.println("\nAeroplanes move in the air..." + "\n");
     }
 }
