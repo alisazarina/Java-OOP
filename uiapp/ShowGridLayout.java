8b2f2a44a74adc28a83bbf86e56b820d942e7c31
Alisa Zarina <85399390+alisazarina@users.noreply.github.com>
2022-05-14 14:20:29 +0800

    // to continue later

---
+++ uiapp/ShowGridLayout.java
@@ -0,0 +1,14 @@
+package uiapp;
+
+/**
+ *
+ * @author Alisa Zarina
+ */
+
+import javax.swing.*;       // JFrame, JButton
+import java.awt.*;          // FlowLayout
+
+
+public class ShowGridLayout extends JFrame {        // inherit values of JFrame
+    
+}
